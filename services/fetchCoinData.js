6abd73e8f105c7761a5bd9090d8a08fd4b84540e
Lalit <lalitmehra0954@gmail.com>
2025-07-19 00:56:43 +0530

    Added react query config

--- services/fetchCoinData.js
+++ services/fetchCoinData.js
@@ -1,9 +1,12 @@
 import axios from "axios";
 import axiosInstance from "../helper/axios";
 
-export async function fetchCoinData() {
+export async function fetchCoinData(page, currency) {
+  const perPage = 10;
   try {
-    const response = await axiosInstance.get("/coins/markets?vs_currency=usd");
+    const response = await axiosInstance.get(
+      `/coins/markets?vs_currency=${currency}&order=market_cap_desc&per_page=${perPage}&page=${page}`
+    );
     console.log(response);
     return response;
   } catch (error) {
--- src/componets/CoinsTable/CoinsTable.jsx
+++ src/componets/CoinsTable/CoinsTable.jsx
@@ -1,7 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { fetchCoinData } from "../../../services/fetchCoinData";
+import { useQuery } from "@tanstack/react-query";
 
 const CoinsTable = () => {
-  return <div>CoinTable</div>;
+  const [page, setPage] = useState(1);
+
+  const { data, isLoading, isError, error } = useQuery({
+    queryKey: ["coins", page],
+    queryFn: () => fetchCoinData(page, "usd"),
+
+    retry: 2,
+    retryDelay: 1000,
+    cacheTime: 1000 * 60 * 2,
+  });
+
+  useEffect(() => {
+    console.log(data), [data];
+  });
+
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+  if (isError) {
+    console.error("Query Errror :", error);
+    return <div>Error:{error.message}.</div>;
+  }
+
+  return (
+    <div>
+      CoinTable <button onClick={() => setPage(page + 1)}>Click</button> {page};
+    </div>
+  );
 };
 
 export default CoinsTable;
